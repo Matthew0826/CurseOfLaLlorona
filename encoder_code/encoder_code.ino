a1d92890e238bef8a97720f2c05c8a62d58d8f7c
Matthew0826 <DoubleEclipse8264@gmail.com>
2022-12-06 22:21:04 -0500

    Made some updates to the code

--- encoder_code/encoder_code.ino
+++ encoder_code/encoder_code.ino
@@ -9,9 +9,6 @@
  #define inputCLK 4
  #define inputDT 7
 
-
-
-
 #define numbPieces 4
 
  #define moveConst 3
@@ -35,7 +32,7 @@ DFRobotDFPlayerMini player;
 
  bool usedCard = false;
  bool breakout = false;
-
+ int deadPlayer = 0;
 
  int ids[numbPieces][2] = {{0, 0}, {0, 0}, {0, 0}, {0, 0}};
  int prevIds[numbPieces][2] = {{0, 0}, {0, 0}, {0, 0}, {0, 0}};
@@ -50,14 +47,15 @@ double laLlorona[2] = {0, 0};
  const int rowSize[6] = {11, 8, 7, 8, 5};
  const char rows[6] = {'A', 'B', 'C', 'D', 'E' };
  const char buttons[numbPieces] = {1, 8, 10, 11};
- bool alive[numbPieces] = {true, true, true, true};
+ bool alive[numbPieces] = {false, false, false, false};
 const String buttonString[numbPieces] = {"Yellow", "Red", "Blue", "Green"};
 
-int currentButton = 0;
+int currentButton = -1;
 
 SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
 
+//These are all of the coordinates for the spaces. 
 const double coords[6][12][2] = {{{0, 0}, {0.5, 1.5}, {0.5, 2.5}, {0.5, 3.5}, {0.5, 4.5}, {0.5, 5.5}, {0.5, 6.5}, {0.75, 7.5}, {1.75, 7.5}, {2.35, 6.2 }, {2.85, 5}, {3.85,5.3 } },
 {{1.25, 1.25}, {2.25, 2.25}, {3.25, 3.25}, {3.75, 4}, {4.5, 4.5}, {5.5, 4.25}, {6.5, 4}, {7.5, 3.5}, {8.5, 3.25} },
 {{1.75, 0.5}, {3, 0.5}, {4.5, 0.5}, {5.5, 0.75}, {6.5, 0.75}, {7.5, 0.75}, {8.5, 0.75}, {8.5, 2} },
@@ -66,6 +64,7 @@ const double coords[6][12][2] = {{{0, 0}, {0.5, 1.5}, {0.5, 2.5}, {0.5, 3.5}, {0
 
  String encdir ="";
 void updateLights(){
+  
   double minDistance = 1000;
    for( int i = 0; i < numbPieces; i++ ){
      if( !alive[i] ){
@@ -81,27 +80,20 @@ void updateLights(){
       }
       int brightness = 0;
       if( distance <= 1.25 ){
-        breakout = true;
         lcd.clear();
-        lcd.print( buttonString[i] );
-        lcd.print( " is Dead!" );
-        lcd.setCursor( 0, 1 );
-        lcd.print( "Used Card? " );
-        if( usedCard ){
-          lcd.print( "Y" );
-        }else{
-          lcd.print( "N" );
-        }
+        lcd.print( "YOU DEAD" );
+        deadPlayer = i;
+        breakout = true;
         brightness = 0;
         alive[i] = false;
-      }else if( distance <= 2.5 ){
+      }else if( distance <= 3 ){
         brightness = 255;
       }
-      else if( distance <= 3 ){
+      else if( distance <= 4 ){
         brightness = 100;
-      }else if( distance <= 3.5 ){
+      }else if( distance <= 5 ){
         brightness = 25;
-      }else if( distance <= 4 ){
+      }else if( distance <= 6 ){
         brightness = 15;
       }else{
         brightness = 5;
@@ -111,43 +103,66 @@ void updateLights(){
    player.volume( (int)(( 2 / minDistance ) * 30) );
  }
  void setup() { 
-
-   laLlorona[0] = random(7, 8);
-   laLlorona[1] = random(7, 8 );
-   lcd.begin(16, 2);
-  lcd.print("hello, world!");
-   // Set encoder pins as inputs  
+   // Setup Serial Monitor
+   Serial.begin(9600);
+     softwareSerial.begin(9600);
+// Start communication with DFPlayer Mini
+  player.begin(softwareSerial);
+  player.stop();
+  
    pinMode (inputCLK,INPUT);
    pinMode (inputDT,INPUT);
    pinMode(inputSW, INPUT_PULLUP);
    pinMode( buttons[0], INPUT_PULLUP );
-    pinMode( buttons[1], INPUT_PULLUP );
-    pinMode( buttons[2], INPUT_PULLUP );
-    pinMode( buttons[3], INPUT_PULLUP );
-
    // Set LED pins as outputs
    pinMode (ledPins[0],OUTPUT);
    pinMode (ledPins[1],OUTPUT);
   pinMode (ledPins[2],OUTPUT);
 
    pinMode (ledPins[3],OUTPUT);
+    pinMode( buttons[1], INPUT_PULLUP );
+    pinMode( buttons[2], INPUT_PULLUP );
+    pinMode( buttons[3], INPUT_PULLUP );
+    lcd.clear();
+    lcd.print("Select Players");
+    //lcd.print("Who will be playing?");
+
+    bool buttonPressed[4] = {false, false, false, false};
+   while( digitalRead( inputSW ) == HIGH || currentButton == -1 ){
+     currentButton = -1;
+     for( int i = 0; i < 4; i++ ){
+       if( digitalRead( buttons[i] ) == LOW && !buttonPressed[i] ){
+         alive[i] = !alive[i];
+          buttonPressed[i] = true;
+       }else if( digitalRead( buttons[i] ) == HIGH ){
+         buttonPressed[i] = false;
+       }
+
+       if( alive[i] && currentButton == -1 ){
+         currentButton = i;
+       }
+        digitalWrite( ledPins[i], alive[i] );
+     }     
+     delay( 10 );
+   }
+
+   for( int i = 0; i < 4; i++ ){
+        digitalWrite( ledPins[i], 0 );
+     }   
+
+
+   laLlorona[0] = random(7, 8);
+   laLlorona[1] = random(7, 8 );
+   lcd.begin(16, 2);
+   // Set encoder pins as inputs  
+   
 
    
-   // Setup Serial Monitor
-   //Serial.begin(9600);
-     softwareSerial.begin(9600);
-// Start communication with DFPlayer Mini
-  if (player.begin(softwareSerial)) {
-   Serial.println("OK");
 
-    // Set volume to maximum (0 to 30).
-    // Play the first MP3 file on the SD card
-    player.volume(5);
+     player.volume(5);
 
     player.loop(1);
-  } else {
-    Serial.println("Connecting to DFPlayer Mini failed!");
-  }
+   
 /*
     mySoftwareSerial.begin(9600);
     if( !myDFPlayer.begin(mySoftwareSerial) ){
@@ -168,7 +183,6 @@ void updateLights(){
 
  
  void moveLaLlorona(){
-  Serial.println( "TEST" );
   double dx = 0;
   double dy = 0;
   for( int i = 0; i < numbPieces; i++ ){
@@ -190,7 +204,17 @@ void updateLights(){
     dy -= ( ( b * noise ) / distance ) * 1.5;
 
   }
+  if( dx >= 1.5 ){
+    dx = 1.5;
+  }else if( dx <= 1.5 ){
+    dx = -1.5;
+  }
 
+  if( dy >= 1.5 ){
+    dy = 1.5;
+  }else if( dy <= 1.5 ){
+    dy = -1.5;
+  }
   laLlorona[0] += dx;
   laLlorona[1] += dy;
 
@@ -219,7 +243,22 @@ void updateLights(){
   // Read the current state of inputCLK
    currentStateCLK = digitalRead(inputCLK);
     if( digitalRead( buttons[currentButton]) == LOW ){
-      Serial.println( "PRESSED!" );
+      lcd.clear();
+      Serial.print(coords[ids[currentButton][1]][ids[currentButton][0]][0]  ); Serial.print( ", " ); Serial.println(coords[ids[currentButton][1]][ids[currentButton][0]][1]);
+      if( ids[currentButton][0] == 5 && ids[currentButton][1] == 4 ){
+        lcd.setCursor( 0, 0 );
+        lcd.print( buttonString[currentButton] ); lcd.print( " escaped..." );
+        lcd.setCursor( 0, 1 );
+        lcd.print( "Rest o yall dead" );
+        player.stop();
+        digitalWrite( ledPins[0], 0 );
+                digitalWrite( ledPins[1], 0 );
+
+        digitalWrite( ledPins[2], 0 );
+
+        digitalWrite( ledPins[3], 0 );
+        exit(0);
+      }
       int startingButton = currentButton;
       currentButton++;
       if( currentButton >= numbPieces ){
@@ -232,11 +271,15 @@ void updateLights(){
         currentButton++;
         if( currentButton >= numbPieces ){
           currentButton = 0;
+          moveLaLlorona();
         }
         if( currentButton == startingButton ){
           //Add some quirky everyone loses code :D
           break;
         }
+      }
+      if( startingButton < currentButton ){
+
       }
       updateLights();
       prevIds[currentButton][0] = ids[currentButton][0];
@@ -249,9 +292,15 @@ void updateLights(){
      if (digitalRead(inputDT) != currentStateCLK) { 
        counter ++;
        if( counter >= moveConst ){
-         counter = 0;
-         ids[currentButton][modCol]++;
+         if( breakout ){
+          usedCard = !usedCard;
+         }else{
+          ids[currentButton][modCol]++;           
+         }
+        counter = 0;
+
        }
+
        encdir ="CCW";
        
      } else {
@@ -260,8 +309,14 @@ void updateLights(){
        encdir ="CW";
 
         if( counter <= -moveConst ){
-         counter = 0;
-         ids[currentButton][modCol]--;
+          if( breakout ){
+            usedCard = !usedCard;
+          }else{
+            ids[currentButton][modCol]--;
+          }
+          counter = 0;
+
+         
        }
        
      }
@@ -275,7 +330,15 @@ void updateLights(){
   //If we detect LOW signal, button is pressed
     if (btnState == LOW) {
       if( breakout ){
+        if( usedCard ){
+          alive[deadPlayer] = true;
+          ids[deadPlayer][0] = 0; ids[deadPlayer][1] = 0;
+          prevIds[deadPlayer][0] = 0; prevIds[deadPlayer][1] = 0;
+          updateLights();
+          player.stop();
+        }
        breakout = false;
+       lcd.clear();
      }
     //since 50ms have passed since last LOW pulse, it means that the
     //button has been pressed, released and pressed again
@@ -290,9 +353,22 @@ void updateLights(){
   // Put in a slight delay to help debounce the reading
   
   //This updates regularly & is our main code body
-  if( ((double)millis() / 1000 ) - lastMillis > 0.1 && !breakout ){
+  if( ((double)millis() / 1000 ) - lastMillis > 0.05 ){
+    if( breakout ){
+        lcd.setCursor(0, 0);
+
+        lcd.print( buttonString[deadPlayer] );        
+        lcd.print( " is Dead!" );
+        lcd.setCursor( 0, 1 );
+        lcd.print( "Used Card? " );
+        if( usedCard ){
+          lcd.print( "Y" );
+        }else{
+          lcd.print( "N" );
+        }
+    }else{
     checkCounter();
-    lcd.clear();
+    //lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print( "Turn: " ); lcd.print( buttonString[currentButton]);
     lcd.setCursor(0, 1);
@@ -300,6 +376,8 @@ void updateLights(){
     //lcd.print( "( " ); lcd.print( coords[ids[1]][ids[0]][0] );lcd.print( ", " ); 
     //lcd.print( coords[ids[1]][ids[0]][1] );lcd.print( " )" );
     lastMillis = (double)millis() / 1000;
+    }
+    
   }
 
   delay(1);
