39a7d0fc383408428d65ae41bca84b6338109d91
Matthew0826 <DoubleEclipse8264@gmail.com>
2022-12-07 01:12:42 -0500

    All the cards work :)

--- encoder_code/encoder_code.ino
+++ encoder_code/encoder_code.ino
@@ -31,12 +31,14 @@ DFRobotDFPlayerMini player;
  double lastMillis = 0;
 
  bool usedCard = false;
+ bool useAbility = false;
  bool breakout = false;
  int deadPlayer = 0;
 
  int ids[numbPieces][2] = {{0, 0}, {0, 0}, {0, 0}, {0, 0}};
  int prevIds[numbPieces][2] = {{0, 0}, {0, 0}, {0, 0}, {0, 0}};
- bool modCol = true;
+ double noises[numbPieces] = {0, 0, 0, 0};
+ int modVal = 0;
  
  String currentDir ="";
  unsigned long lastButtonPress = 0;
@@ -62,7 +64,7 @@ const double coords[6][12][2] = {{{0, 0}, {0.5, 1.5}, {0.5, 2.5}, {0.5, 3.5}, {0
 {{2.75, 8}, {3.75, 8.25}, {4.65, 8.65}, {5.38, 9.25 }, {6.25, 8}, {7,7 }, {7.25, 6}, {6.37, 6.65}, {5.5, 5.25}  },
 {{8.5, 4.5}, {8.5, 5.5}, {8.5, 6.5}, {8.75,7.25 },{9, 8.25}, {9.5, 9.5}}};
 
- String encdir ="";
+String encdir ="";
 void updateLights(){
   
   double minDistance = 1000;
@@ -79,7 +81,7 @@ void updateLights(){
         minDistance = distance;
       }
       int brightness = 0;
-      if( distance <= 1.25 ){
+      if( distance <= 0.5 ){
         lcd.clear();
         lcd.print( "YOU DEAD" );
         deadPlayer = i;
@@ -104,12 +106,13 @@ void updateLights(){
  }
  void setup() { 
    // Setup Serial Monitor
-   Serial.begin(9600);
+   //Serial.begin(9600);
      softwareSerial.begin(9600);
 // Start communication with DFPlayer Mini
   player.begin(softwareSerial);
   player.stop();
-  
+  pinMode( 0, INPUT );
+  randomSeed(analogRead(0));
    pinMode (inputCLK,INPUT);
    pinMode (inputDT,INPUT);
    pinMode(inputSW, INPUT_PULLUP);
@@ -151,17 +154,13 @@ void updateLights(){
      }   
 
 
-   laLlorona[0] = random(7, 8);
-   laLlorona[1] = random(7, 8 );
+   laLlorona[0] = random(3, 10);
+   laLlorona[1] = random(3, 10 );
    lcd.begin(16, 2);
    // Set encoder pins as inputs  
-   
 
-   
-
-     player.volume(5);
-
-    player.loop(1);
+   player.volume(5);
+   player.loop(1);
    
 /*
     mySoftwareSerial.begin(9600);
@@ -177,46 +176,54 @@ void updateLights(){
    // Assign to previousStateCLK variable
    previousStateCLK = digitalRead(inputCLK);
   updateLights();
-    randomSeed(millis());
+  randomSeed(millis());
 
  } 
 
  
  void moveLaLlorona(){
+  Serial.print( laLlorona[0] ); Serial.print( ", " ); Serial.println( laLlorona[1] );
+
   double dx = 0;
   double dy = 0;
   for( int i = 0; i < numbPieces; i++ ){
     if( !alive[i] ){
       continue;
     }
+    noises[i] *= 0.75;
     //Serial.print( i ); Serial.print( ": ");Serial.print(coords[ids[i][1]][ids[i][0]][0] );
     //Serial.print( ", " ); Serial.println( coords[ids[i][1]][ids[i][0]][1] );
     double noise = pow( coords[ids[i][1]][ids[i][0]][0] - coords[prevIds[i][1]][prevIds[i][0]][0], 2) + 
     pow( coords[ids[i][1]][ids[i][0]][1] - coords[prevIds[i][1]][prevIds[i][0]][1], 2);
     noise = sqrt( noise );
-
-
+    noises[i] += noise;
+    Serial.print( "Noise: " );
+    Serial.println( noises[i] );
+    Serial.print(coords[ids[i][1]][ids[i][0]][0]  ); Serial.print( ", " ); Serial.println(coords[ids[i][1]][ids[i][0]][1]);
     int a = laLlorona[0] - coords[ids[i][1]][ids[i][0]][0];
     int b = laLlorona[1] - coords[ids[i][1]][ids[i][0]][1];
+    Serial.println( a );
+    if( a > 0 ){
+      dx += noises[i];
+    }else if( a < 0 ){
+      dx -= noises[i];
+    }    
+    Serial.println( dx );
+    Serial.println( dy );
+    Serial.println();
 
-    double distance = pow( a, 2) + pow( b, 2);
-    dx -= ( ( a * noise ) / distance ) * 1.5;
-    dy -= ( ( b * noise ) / distance ) * 1.5;
+    if( b > 0 ){
+      dy += noises[i];
+    }else if( b < 0 ){
+      dy -= noises[i];
+    } 
 
+    prevIds[i][0] = ids[i][0]; prevIds[i][1] = ids[i][1];
   }
-  if( dx >= 1.5 ){
-    dx = 1.5;
-  }else if( dx <= 1.5 ){
-    dx = -1.5;
-  }
-
-  if( dy >= 1.5 ){
-    dy = 1.5;
-  }else if( dy <= 1.5 ){
-    dy = -1.5;
-  }
-  laLlorona[0] += dx;
-  laLlorona[1] += dy;
+  double magnitude = sqrt( (dx * dx) + (dy * dy) );
+  
+  laLlorona[0] -= ( dx / magnitude ) * 0.75;
+  laLlorona[1] -= ( dy / magnitude ) * 0.75;
 
   Serial.print( laLlorona[0] ); Serial.print( ", " ); Serial.println( laLlorona[1] );
    
@@ -244,7 +251,9 @@ void updateLights(){
    currentStateCLK = digitalRead(inputCLK);
     if( digitalRead( buttons[currentButton]) == LOW ){
       lcd.clear();
-      Serial.print(coords[ids[currentButton][1]][ids[currentButton][0]][0]  ); Serial.print( ", " ); Serial.println(coords[ids[currentButton][1]][ids[currentButton][0]][1]);
+      if( ids[currentButton][0] == prevIds[currentButton][0] && ids[currentButton][1] == prevIds[currentButton][1] ){
+        noises[currentButton] += 3;
+      }
       if( ids[currentButton][0] == 5 && ids[currentButton][1] == 4 ){
         lcd.setCursor( 0, 0 );
         lcd.print( buttonString[currentButton] ); lcd.print( " escaped..." );
@@ -262,7 +271,6 @@ void updateLights(){
       int startingButton = currentButton;
       currentButton++;
       if( currentButton >= numbPieces ){
-        moveLaLlorona();
         currentButton = 0;
       }
       
@@ -271,16 +279,14 @@ void updateLights(){
         currentButton++;
         if( currentButton >= numbPieces ){
           currentButton = 0;
-          moveLaLlorona();
         }
         if( currentButton == startingButton ){
           //Add some quirky everyone loses code :D
           break;
         }
       }
-      if( startingButton < currentButton ){
+      moveLaLlorona();
 
-      }
       updateLights();
       prevIds[currentButton][0] = ids[currentButton][0];
       prevIds[currentButton][1] = ids[currentButton][1];
@@ -291,11 +297,17 @@ void updateLights(){
      // the encoder is rotating counterclockwise
      if (digitalRead(inputDT) != currentStateCLK) { 
        counter ++;
+       lcd.clear();
        if( counter >= moveConst ){
          if( breakout ){
           usedCard = !usedCard;
          }else{
-          ids[currentButton][modCol]++;           
+           if( modVal < 2 ){
+            ids[currentButton][modVal]++; 
+           }else{
+             useAbility = !useAbility;
+           }
+                      
          }
         counter = 0;
 
@@ -305,6 +317,7 @@ void updateLights(){
        
      } else {
        // Encoder is rotating clockwise
+       lcd.clear();
        counter --;
        encdir ="CW";
 
@@ -312,11 +325,13 @@ void updateLights(){
           if( breakout ){
             usedCard = !usedCard;
           }else{
-            ids[currentButton][modCol]--;
+            if( modVal < 2 ){
+              ids[currentButton][modVal]--; 
+            }else{
+              useAbility = !useAbility;
+            }
           }
-          counter = 0;
-
-         
+          counter = 0;    
        }
        
      }
@@ -343,7 +358,16 @@ void updateLights(){
     //since 50ms have passed since last LOW pulse, it means that the
     //button has been pressed, released and pressed again
     else if (millis() - lastButtonPress > 50) {
-      modCol = !modCol;
+      lcd.clear();
+      if( modVal == 3 && useAbility ){
+        noises[currentButton] += 6;
+        useAbility = false;
+      }
+      modVal++;
+      if( modVal > 3 ){
+        modVal = 0;
+      }
+      
     }
 
 
@@ -367,18 +391,48 @@ void updateLights(){
           lcd.print( "N" );
         }
     }else{
-    checkCounter();
-    //lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print( "Turn: " ); lcd.print( buttonString[currentButton]);
-    lcd.setCursor(0, 1);
-    lcd.print(rows[ids[currentButton][1]]);lcd.print(ids[currentButton][0] + 1);
-    //lcd.print( "( " ); lcd.print( coords[ids[1]][ids[0]][0] );lcd.print( ", " ); 
-    //lcd.print( coords[ids[1]][ids[0]][1] );lcd.print( " )" );
-    lastMillis = (double)millis() / 1000;
+      checkCounter();
+      //lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print( "Turn: " ); lcd.print( buttonString[currentButton]);
+      lcd.setCursor(0, 1);
+      if( modVal < 2 ){
+        lcd.print(rows[ids[currentButton][1]]);lcd.print(ids[currentButton][0] + 1);   
+      }else if( modVal == 2 ){
+        lcd.print( "Switch? " );
+        if( useAbility ){
+          lcd.print( "Y" );
+          for( int i = 0; i < 4; i++ ){
+            if( digitalRead( buttons[i] ) == LOW ){
+              int tempId0 = ids[currentButton][0];     int tempId1 = ids[currentButton][1];
+              ids[currentButton][0] = ids[i][0];  ids[currentButton][1] = ids[i][1];
+              ids[i][0] = tempId0;        ids[i][1] = tempId1;
+
+              prevIds[i][0] = ids[i][0];   prevIds[i][1] = ids[i][1];
+              prevIds[currentButton][0] = ids[i][0];   prevIds[currentButton][1] = ids[i][1];     
+              useAbility = false;
+              modVal = 0;    
+              lcd.clear();
+            }
+          }
+        }else{
+          lcd.print( "N" );
+        }
+      }else{
+        lcd.print( "Make noise? " );
+        if( useAbility ){
+          lcd.print( "Y" );
+        }else{
+          lcd.print( "N" );
+        }
+      }
+      //lcd.print( "( " ); lcd.print( coords[ids[1]][ids[0]][0] );lcd.print( ", " ); 
+      //lcd.print( coords[ids[1]][ids[0]][1] );lcd.print( " )" );
+      lastMillis = (double)millis() / 1000;
     }
     
   }
+
 
   delay(1);
 }
